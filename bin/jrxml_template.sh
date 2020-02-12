48ee8c6d35333815f918d0fd525db9aa3e4ea647
Prashant Maroli <prashant.maroli@evolvussolutions.com>
2020-02-12 17:21:59 +0400

    Update jrxml_template.sh

--- bin/jrxml_template.sh
+++ bin/jrxml_template.sh
@@ -4,11 +4,17 @@ TABLE=$1
 HEADER=$2
 if [ $DB_TYPE = "MYSQL" ]
 then
-    mapfile -t collist < <(mysql -B --column-names=0 -h127.0.0.1 -uroot -pevolvus*123 -e "SELECT concat(COLUMN_NAME,'|',least(200,case when data_type not in ('VARCHAR','CHAR') then 200 else character_maximum_length end),'|',data_type)  FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'project_management' AND TABLE_NAME = 'products' order by ORDINAL_POSITION;")
+    mapfile -t collist < <(mysql -B --column-names=0 -h127.0.0.1 -uroot -pevolvus*123 -e "SELECT concat(COLUMN_NAME,'|',least(200,case when data_type not in ('VARCHAR','CHAR') then 200 else character_maximum_length end),'|',data_type)  FROM INFORMATION_SCHEMA.COLUMNS WHERE   TABLE_NAME = '$TABLE'  order by ORDINAL_POSITION;")
 
 elif [ $DB_TYPE = "ORACLE" ]
 then
-    collist=($(sqlplus -s precision/password@GLFNCMI<<eof
+    collist=($(sqlplus -s precision/987#654#32Masterlove@GLFNCMI<<eof
+	 set pages 0
+	 set head off
+	 set feed off
+	 select column_name||'|'||200||'|'||data_type||'|'||nvl(data_scale,0) from all_tab_columns where table_name = '$TABLE'  order by column_id;
+    exit
+    eof))
 fi
 set pages 0
 set head off
